d6f6eb405f1041d6afcbf468f713b43988eb9778
NORMss <serega.b301@gmail.com>
2025-02-26 23:57:07 +0700

    init project

---
+++ app/src/main/java/ru/normno/mysinglestatevsstatedataclass/MainViewModel.kt
@@ -0,0 +1,16 @@
+package ru.normno.mysinglestatevsstatedataclass
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainViewModel : ViewModel() {
+    private val _state = MutableStateFlow(MainState())
+    val state = _state.asStateFlow()
+
+    private val _email = MutableStateFlow("")
+    val email = _email.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password = _password.asStateFlow()
+}
---
+++ app/src/main/java/ru/normno/mysinglestatevsstatedataclass/MianState.kt
@@ -0,0 +1,6 @@
+package ru.normno.mysinglestatevsstatedataclass
+
+data class MainState(
+    val email: String = "",
+    val password: String = "",
+)
